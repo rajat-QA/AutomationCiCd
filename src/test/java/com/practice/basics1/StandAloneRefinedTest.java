1d5dd6c566f357b24927f6601a3c09b3a6291b2e
Rajat Mahadik <rajat.mahadik@gupshup.io>
2025-04-06 10:25:52 +0530

    Test file changes for CI CD demo 2

--- src/test/java/com/practice/basics1/StandAloneRefinedTest.java
+++ src/test/java/com/practice/basics1/StandAloneRefinedTest.java
@@ -9,14 +9,14 @@ import java.util.HashMap;
 public class StandAloneRefinedTest extends BaseTest {
 
 
-//    @Test(testName = "Get product details", dataProviderClass = eComDataProvider.class, dataProvider = "ecommerceData",  enabled = true, priority = 1)
+    @Test(testName = "Get product details", dataProviderClass = eComDataProvider.class, dataProvider = "ecommerceData",  enabled = true, priority = 1)
     public void getTheProductDetails(HashMap<String, String> edata) throws InterruptedException {
         logging("INFO","  ******** "+ Thread.currentThread().getStackTrace()[1].getMethodName() +" ******* ");
         testExecution.landingPage().loginIntoApp(edata.get("url"), edata.get("email"), edata.get("password"));
         testExecution.productDetailsPage().selectProduct(edata.get("productName"));
     }
 
-    @Test(testName = "Select the product and place order", dataProviderClass = eComDataProvider.class, dataProvider = "ecommerceData", enabled = true, priority = 2)
+//    @Test(testName = "Select the product and place order", dataProviderClass = eComDataProvider.class, dataProvider = "ecommerceData", enabled = true, priority = 2)
     public void getTheOrderDetails(HashMap<String, String> edata) throws InterruptedException {
         testExecution.landingPage().loginIntoApp(edata.get("url"), edata.get("email"), edata.get("password"));
         testExecution.productDetailsPage().selectProduct(edata.get("productName"));
